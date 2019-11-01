0da19b0766ce893c72e69e57f0c435be5b7bd12b
Melkus Adam <Adam.Melkus@ysoft.com>
2019-11-01 23:25:29 +0100

    Added ccat

--- java/path.zsh
+++ java/path.zsh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ## SDKMAN
 export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "/Users/melkus/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/melkus/.sdkman/bin/sdkman-init.sh"
+[[ -s "~/.sdkman/bin/sdkman-init.sh" ]] && source "~/.sdkman/bin/sdkman-init.sh"
